e9ad6b6f25a5bc6495bb61565061c227161dba0e
Leonardo Jaquimenute Gomes <leonardo.jaquimenute@gmail.com>
2025-09-10 17:53:10 -0400

    commit

--- src/main/java/ContaTerminal.java
+++ src/main/java/ContaTerminal.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.println("Insira o numero da conta: ");
+        int numeroDaConta = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Insira o numero da agencia: ");
+        String numeroDaAgencia = sc.nextLine();
+
+        System.out.println("Insira o seu nome: ");
+        String nomeDoCliente = sc.nextLine();
+
+        System.out.println("Insira o seu Saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é: "
+                + numeroDaAgencia + ", conta: " + numeroDaConta + " e seu saldo: " + saldo + " já está disponível para saque.");
+    }
 }
